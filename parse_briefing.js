e3bd3843109518e6f62737e9b09149875d0887c6
omniaura <omniaura5@gmail.com>
2017-03-26 16:47:39 -0700

    update parser and dummy data

--- parse_briefing.js
+++ parse_briefing.js
@@ -5,12 +5,12 @@ const cheerio = require('cheerio');
 const jsonfile = require('jsonfile');
 
 request
-.get('http://mobile.nytimes.com/2016/06/30/nytnow/your-thursday-briefing-istanbul-supreme-court-michael-phelps.html')
+.get('http://mobile.nytimes.com/2016/07/08/nytnow/your-friday-briefing-dallas-donald-trump-euro-2016.html')
 .end((err, res) => {
   if (err) console.error(err);
   const $ = cheerio.load(res.text);
 
-  let document = {
+  let briefing = {
     headline: $('.headline').text(),
     byline: $('#byline').text(),
     date: Date($('.dateline')),
@@ -25,66 +25,91 @@ request
   paragraphs.each(function(index, paragraph) {
     if (finished) return;
 
-    let text = $(this).text(); 
-    let isImage = $(this).hasClass('span-image');
+    const text = $(this).text();
+    const currentLastBrief = briefing.briefs[briefing.briefs.length-1];
 
-    // stop condition
+    // stop condition: don't parse briefs after Business section
     if (text === 'Business') {
       finished = true;
     }
-    // brief title
-    else if (text.indexOf('•') === 0) {
-      document.briefs.push({
-        title: text.substr(2),
+    // new brief: paragraph starts with a bullet and contains bolded text
+    else if (text.indexOf('•') === 0 && $(this).find('strong').length) {
+      briefing.briefs.push({
+        title: text.substr(2), // cut off the bullet and the space following it
         sentences: [],
         images: []
       });
     }
     // we haven't reached the first bullet yet; continue 
-    else if (!document.briefs.length) {
+    else if (!briefing.briefs.length) {
     }
     // inline image
-    else if (isImage) {
-      document.briefs[document.briefs.length-1].images.push(
+    else if ($(this).hasClass('span-image')) {
+      currentLastBrief.images.push(
         $(this).find('img').attr('src')
       );
     }
     // regular text paragraph
     else {
       let sentences = text.split('. ');
-      sentences = sentences.forEach(sentence => {
-        document.briefs[document.briefs.length-1].sentences.push(sentence);
+      let links = desktopLinksToMobile($, $(this).find('a')); // links in this paragraph
+      sentences.forEach(sentence => {
+        currentLastBrief.sentences.push({
+          text: sentence,
+          media: [] // image or video URLs to be populated later
+        });
+
+        // go through each link in the paragraph and find its associated sentence 
+        links.each(function(index, link) {
+          const url = $(this).prop('href');
+          // if the text of this link is found in this sentence and the link is an NYT link
+          if (sentence.indexOf($(this).text()) > -1 && url.indexOf('mobile.nytimes.com') > -1) {
+            getArticleMedia(url, 
+                            currentLastBrief.sentences[currentLastBrief.sentences.length-1]);
+          }
+        });
       });
-      getImagesFromLinks($, $(this), 
-                         document.briefs[document.briefs.length-1].images);
     }
   });
   setTimeout(() => { 
-    jsonfile.writeFile('briefing.json', document, {spaces: 2}, err => {
+    jsonfile.writeFile('briefing.json', briefing, {spaces: 2}, err => {
       if (err) console.log(err);
     });
-  }, 5000);
+  }, 10000);
 });
 
-function getImagesFromLinks($, paragraph, briefImages) {
-  let links = desktopLinksToMobile($, paragraph.find('a'));
-  links.each(function(index, link) {
-    const url = $(this).prop('href');
-    if (url.indexOf('mobile.nytimes.com') > -1) {
-      request
-      .get(url)
-      .end((err, res) => {
-        if (err) {
-          console.error('error on: ' + url);
-          return;
-        }
-        const $$ = cheerio.load(res.text);
-        const leadPhoto = $$('.span-image')
-        if (leadPhoto) {
-          briefImages.push(leadPhoto.find('img').data('lightbox-image-url'));
-        }
+function getArticleMedia(url, sentence) {
+  request
+  .get(url)
+  .end((err, res) => {
+    if (err) {
+      console.error('error on: ' + url);
+      return;
+    }
+    const $ = cheerio.load(res.text);
+    const leadPhoto = $('header .span-asset-img');
+    const leadVideo = $('header .article-span-video-container');
+    if (leadPhoto.length) {
+      sentence.media.push({
+        type: 'image',
+        url: leadPhoto.find('img').data('lightbox-image-url')
       });
     }
+    if (leadVideo.length) {
+      getVideoUrl(leadVideo.data('video-id'), sentence);
+    }
+  });
+}
+
+function getVideoUrl(id, sentence) {
+  request
+  .get(`https://www.nytimes.com/svc/video/api/v3/video/${id}`)
+  .end((err, res) => {
+    if (err) console.error(err);
+    sentence.media.push({
+      type: 'video',
+      url: res.body.renditions
+    });
   });
 }
 
